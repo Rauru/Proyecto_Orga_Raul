ceb7bf7e7003d66e46002760b7be24381313dcc5
Rauru <raul_lopez_tu@hotmail.com>
2020-09-17 02:49:57 -0600

    movement

--- plugin_main.cpp
+++ plugin_main.cpp
@@ -5,22 +5,116 @@
 extern "C" ErrorCode handleSyscall(uint32_t *regs, void *mem, MemoryMap *mem_map)
 {
     unsigned v0 = regs[Register::v0];
-
+    rlutil::setColor(rlutil::RED);
     switch (v0)
-    {
-        case 20:
+    {   
+        case 0:{
+            rlutil::cls();
+            return ErrorCode::Ok;
+        }
+        case 55:{
+            rlutil::hidecursor();
+            return ErrorCode::Ok;
+        }
+        case 20:{
+            rlutil::hidecursor();
+            int a0 = regs[Register::a0];
+            int a1 = regs[Register::a1];
+            int a2 = regs[Register::a2];
+            int a3 = regs[Register::a3];
+            rlutil::setBackgroundColor(a3);
+            rlutil::locate(a0,a1);
+            rlutil::setChar(a2);
+            std::cout.flush();
+            rlutil::msleep(40);
+            return ErrorCode::Ok;
+        }
+        case 21:{
+          //  RLUTIL_INLINE int nb_getch(	void	)
+           // while(!kbhit()) {}
+            //int getc=  rlutil::nb_getch();
+           // int callgetkey = rlutil::getkey();
+           //std::cout << "getkey: " << getc << '\n' << std::flush;
+            regs[Register::v0] =  rlutil::nb_getch();
+            //rlutil::msleep(5000);
+            return ErrorCode::Ok;
+        }
+        case 222:{
+            int a0 = regs[Register::a0];
+            int a1 = regs[Register::a1];
+            rlutil::cls();
+            rlutil::setBackgroundColor(3);
+            rlutil::locate(a0,a1);
+            rlutil::setChar(35);
+            rlutil::locate(a0+1,a1);
+
+
+            rlutil::setBackgroundColor(3);
+            rlutil::locate(1,1);
+            rlutil::setChar(35);
+            rlutil::locate(2,1);
+            return ErrorCode::Ok;
+
+        }
+        case 122:
         {
             int a0 = regs[Register::a0];
             int a1 = regs[Register::a1];
             regs[Register::v0] = a0 + a1;
             return ErrorCode::Ok;
         }
+        case 231:
+        {
+             rlutil::cls();
+             regs[Register::a0] = rlutil::trows();
+             regs[Register::a1]=rlutil::tcols();
+             return ErrorCode::Ok;
+        }
+        case 232:
+        {   
+            rlutil::cls();
+            
+           rlutil::setBackgroundColor(3);
+            rlutil::locate(20,20);
+
+            rlutil::setChar(32);
+             rlutil::locate(21,20);
+           rlutil::setChar(32);
+           rlutil::locate(22,20);
+           rlutil::setChar(32);
+           rlutil::locate(23,20);
+           rlutil::setChar(32);
+           rlutil::setBackgroundColor(5);
+           rlutil::locate(24,20);
+           rlutil::setChar(32);
+           rlutil::locate(25,20);
+           rlutil::setChar(32);
+
+
+            
+            rlutil::setBackgroundColor(4);
+            rlutil::locate(100,21);
+            rlutil::setChar(35);
+            // rlutil::saveDefaultColor();
+           // rlutil::resetColor();
+
+
+           // rlutil::setBackgroundColor(4);
+            //rlutil::locate(21,21);
+            
+
+           // rlutil::setBackgroundColor(9);
+            //rlutil::locate(40,22);
+           
+            rlutil::setColor(14);
+            return ErrorCode::Ok;
+        }
         default:
-            if (v0 > 20 && v0 <= 50)
+            if (v0 > 100 && v0 <= 102)
             {
-                rlutil::cls();
+                
                 std::cout << "Syscall: " << v0 << '\n' << std::flush;
-                rlutil::cls();
+               
                 rlutil::anykey();
                 return ErrorCode::Ok;
                 
--- test.asm
+++ test.asm
@@ -1,8 +1,102 @@
-li $v0, 21
+addi $sp, $sp, -16
+
+li $t0, 60
+sw $t0, 0($sp)
+
+li $t0, 15
+sw $t0, 4($sp)
+
+li $t0, 1
+sw $t0, 8($sp)
+
+li $v0, 1
 syscall
-li $v0, 22
+
+li $v0, 0
 syscall
-li $v0, 23
+
+
+
+begin:
+li $v0, 0
 syscall
-li $v0, 24
+
+li $t0, 120
+li $t1, 31
+li $t2, 1
+li $a3, 0
+li $a2, 32
+li $t4, 0
+;fory:
+;   beq $t1, $t2, endfory   
+   
+;   li $t3, 1
+;   forx:
+;   beq $t0, $t3, endforx
+   
+;   move $a0, $t3
+;   move $a1, $t2
+;   li $v0, 20
+;   syscall
+   ;#show $t0
+   
+;   addi $t4, $t4, 1
+
+;   addi $t3, $t3, 1
+;   j forx
+;   endforx:
+;   addi $t2, $t2, 1
+;   j fory
+   
+;endfory:
+
+
+draw_ship:
+   li $a2, 35
+   lw $a0, 0($sp)
+   lw $a1, 4($sp)
+   li $a3, 3
+   li $v0, 20
+   syscall
+   li $a2, 35
+   addi $a0, $a0, -1
+   li $v0, 20
+   syscall
+   li $a2, 35
+   addi $a0, $a0, 2
+   lw $a1, 4($sp)
+   li $a3, 3
+   li $v0, 20
+   syscall
+
+
+li $a3, 0
+li $a0, 120
+li $a1, 30
+li $v0, 20
 syscall
+
+
+;#show $t4
+
+
+loop:
+
+;delta:
+ ;  beq $t1, $t2, endfory   
+;enddelta:
+li $v0, 55
+syscall
+
+;li $v0, 21
+;syscall
+;li $t0, 119
+;beq $v0, $t0,
+
+
+lw $a0, 0($sp)
+addi $a0, $a0, -1
+sw $a0, 0($sp)
+j begin
+
+j loop

242a48f2fc868133d5d4c8eb1765cc7e506e66b9
Rauru <raul_lopez_tu@hotmail.com>
2020-09-25 17:43:00 -0600

    all bullet hitboxes

--- test.asm
+++ test.asm
@@ -1,4 +1,4 @@
-addi $sp, $sp, -112
+addi $sp, $sp, -116
 
 li $t0, 40     ;#posX ship
 sw $t0, 0($sp)
@@ -50,47 +50,49 @@ li $t0, 0    ;#ov3 updown switch
 sw $t0, 56($sp)
 ;#--------------bullets-----------------
 li $t0, 200
-sw $t0, 60($sp)
+sw $t0, 60($sp) ;#b1 x
 
 li $t0, 100
-sw $t0, 64($sp)
+sw $t0, 64($sp) ;#b1 y
 
 li $t0, 1
 sw $t0, 68($sp)
 
 li $t0, 200
-sw $t0, 72($sp)
+sw $t0, 72($sp) ;#b2 x
 
 li $t0, 100
-sw $t0, 76($sp)
+sw $t0, 76($sp) ;#b2 y
 
 li $t0, 1
-sw $t0, 80($sp)
+sw $t0, 80($sp) 
 
 li $t0, 200
-sw $t0, 84($sp)
+sw $t0, 84($sp) ;#b3 x
 
 li $t0, 100
-sw $t0, 88($sp)
+sw $t0, 88($sp) ;#b3 y
 
 li $t0, 1
 sw $t0, 92($sp)
 
 li $t0, 200
-sw $t0, 96($sp)
+sw $t0, 96($sp) ;#b4 x
 
 li $t0, 100
-sw $t0, 100($sp)
+sw $t0, 100($sp) ;#b4 y
 
 li $t0, 1
 sw $t0, 104($sp)
 
 li $t0, 1
 sw $t0, 108($sp) ;#which bullet
-
-
-
 ;#------------------------
+
+li $t0, 0
+sw $t0, 112($sp)
+
+
 li $v0, 1
 syscall
 
@@ -528,7 +530,7 @@ not_shoot_right3:
 
 move_bullet3_left:
    bgtz $a0, check_if_left3
-   j not_shoot_left2
+   j not_shoot_left3
 
 check_if_left3:
    lw $t0, 92($sp)
@@ -573,7 +575,7 @@ not_shoot_right4:
 
 move_bullet4_left:
    bgtz $a0, check_if_left4
-   j not_shoot_left2
+   j not_shoot_left4
 
 check_if_left4:
    lw $t0, 104($sp)
@@ -902,6 +904,529 @@ ovn3_collision:
    beq $t1, $t5, game_over
 
 ovni3_done:
+
+ovn4_collision:
+   ;24,28,36, 40
+   lw $t2, 48($sp) ;#posX ovni2
+   lw $t3, 52($sp) ;#posy ovni2
+
+   
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_y4
+   j ovni4_done
+
+   check_y4:
+
+   beq $t1, $t3, game_over
+   beq $t1, $t4, game_over
+   beq $t1, $t5, game_over
+
+ovni4_done:
+
+
+bullets_hitboxes:
+    
+
+   lw $t0, 60($sp) ;#posX bull 1
+   lw $t1, 64($sp)  ;#posY bull 1
+
+bullet1_ovni1:
+   lw $t2, 16($sp) ;#posX ovni1
+   lw $t3, 20($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull_y
+   j ovni1_bull_done
+
+   check_bull_y:
+
+   beq $t1, $t3, scoreup
+   beq $t1, $t4, scoreup
+   beq $t1, $t5, scoreup
+
+   j ovni1_bull_done
+scoreup:
+   li $t2, -20
+   sw $t2, 16($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 64($sp)
+
+ovni1_bull_done:
+
+
+bullet1_ovni2:
+   lw $t2, 24($sp) ;#posX ovni1
+   lw $t3, 28($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull_y2
+   j ovni1_bull_done2
+
+   check_bull_y2:
+
+   beq $t1, $t3, scoreup2
+   beq $t1, $t4, scoreup2
+   beq $t1, $t5, scoreup2
+
+   j ovni1_bull_done2
+scoreup2:
+   li $t2, -20
+   sw $t2, 24($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 64($sp) ;#pos bullet1
+
+ovni1_bull_done2:
+
+
+bullet1_ovni3:
+   lw $t2, 36($sp) ;#posX ovni1
+   lw $t3, 40($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull_y3
+   j ovni1_bull_done3
+
+   check_bull_y3:
+
+   beq $t1, $t3, scoreup3
+   beq $t1, $t4, scoreup3
+   beq $t1, $t5, scoreup3
+
+   j ovni1_bull_done3
+scoreup3:
+   li $t2, -20
+   sw $t2, 36($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 64($sp) ;#pos bullet1
+
+ovni1_bull_done3:
+
+
+
+bullet1_ovni4:
+   lw $t2, 48($sp) ;#posX ovni1
+   lw $t3, 52($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull_y4
+   j ovni1_bull_done4
+
+   check_bull_y4:
+
+   beq $t1, $t3, scoreup4
+   beq $t1, $t4, scoreup4
+   beq $t1, $t5, scoreup4
+
+   j ovni1_bull_done4
+scoreup4:
+   li $t2, -20
+   sw $t2, 48($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 64($sp) ;#pos bullet1
+
+ovni1_bull_done4:
+
+;# 60,64, 72,76,  84,88, 96,100 bullets
+    ;#16,20 24,28, 36,40, 48,52 ovnis
+    ;#score 112
+    
+   lw $t0, 72($sp) ;#posX bull 1
+   lw $t1, 76($sp)  ;#posY bull 1
+
+bullet2_ovni:
+   lw $t2, 16($sp) ;#posX ovni1
+   lw $t3, 20($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull2_y
+   j ovni1_bull2_done
+
+   check_bull2_y:
+
+   beq $t1, $t3, scoreup_bull2
+   beq $t1, $t4, scoreup_bull2
+   beq $t1, $t5, scoreup_bull2
+
+   j ovni1_bull2_done
+scoreup_bull2:
+   li $t2, -20
+   sw $t2, 16($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 76($sp) ;#pos bullet1
+
+ovni1_bull2_done:
+
+
+
+bullet2_ovni2:
+   lw $t2, 24($sp) ;#posX ovni1
+   lw $t3, 28($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull2_y2
+   j ovni1_bull2_done2
+
+   check_bull2_y2:
+
+   beq $t1, $t3, scoreup2_bull2
+   beq $t1, $t4, scoreup2_bull2
+   beq $t1, $t5, scoreup2_bull2
+
+   j ovni1_bull2_done2
+scoreup2_bull2:
+   li $t2, -20
+   sw $t2, 24($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 76($sp) ;#pos bullet1
+
+ovni1_bull2_done2:
+
+bullet2_ovni3:
+   lw $t2, 36($sp) ;#posX ovni1
+   lw $t3, 40($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull2_y3
+   j ovni1_bull2_done3
+
+   check_bull2_y3:
+
+   beq $t1, $t3, scoreup3_bull2
+   beq $t1, $t4, scoreup3_bull2
+   beq $t1, $t5, scoreup3_bull2
+
+   j ovni1_bull2_done3
+scoreup3_bull2:
+   li $t2, -20
+   sw $t2, 36($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 76($sp) ;#pos bullet1
+
+ovni1_bull2_done3:
+
+
+
+bullet2_ovni4:
+   lw $t2, 48($sp) ;#posX ovni1
+   lw $t3, 52($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull2_y4
+   j ovni1_bull2_done4
+
+   check_bull2_y4:
+
+   beq $t1, $t3, scoreup4_bull2
+   beq $t1, $t4, scoreup4_bull2
+   beq $t1, $t5, scoreup4_bull2
+
+   j ovni1_bull2_done4
+scoreup4_bull2:
+   li $t2, -20
+   sw $t2, 36($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 76($sp) ;#pos bullet1
+
+ovni1_bull2_done4:
+
+    ;# 60,64, 72,76,  84,88, 96,100 bullets
+    ;#16,20 24,28, 36,40, 48,52 ovnis
+    ;#score 112
+
+    
+
+    
+   lw $t0, 84($sp) ;#posX bull 1
+   lw $t1, 88($sp)  ;#posY bull 1
+
+bullet3_ovni:
+   lw $t2, 16($sp) ;#posX ovni1
+   lw $t3, 20($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull3_y
+   j ovni1_bull3_done
+
+   check_bull3_y:
+
+   beq $t1, $t3, scoreup_bull3
+   beq $t1, $t4, scoreup_bull3
+   beq $t1, $t5, scoreup_bull3
+
+   j ovni1_bull3_done
+scoreup_bull3:
+   li $t2, -20
+   sw $t2, 16($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 88($sp) ;#pos bullet1
+
+ovni1_bull3_done:
+
+;# 60,64, 72,76,  84,88, 96,100 bullets
+    ;#16,20 24,28, 36,40, 48,52 ovnis
+    ;#score 112
+
+bullet3_ovni2:
+   lw $t2, 24($sp) ;#posX ovni1
+   lw $t3, 28($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull3_y2
+   j ovni1_bull3_done2
+
+   check_bull3_y2:
+
+   beq $t1, $t3, scoreup2_bull3
+   beq $t1, $t4, scoreup2_bull3
+   beq $t1, $t5, scoreup2_bull3
+
+   j ovni1_bull3_done2
+scoreup2_bull3:
+   li $t2, -20
+   sw $t2, 24($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 88($sp) ;#pos bullet1
+
+ovni1_bull3_done2:
+
+
+;# 60,64, 72,76,  84,88, 96,100 bullets
+    ;#16,20 24,28, 36,40, 48,52 ovnis
+    ;#score 112
+
+bullet3_ovni3:
+   lw $t2, 36($sp) ;#posX ovni1
+   lw $t3, 40($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull3_y3
+   j ovni1_bull3_done3
+
+   check_bull3_y3:
+
+   beq $t1, $t3, scoreup3_bull3
+   beq $t1, $t4, scoreup3_bull3
+   beq $t1, $t5, scoreup3_bull3
+
+   j ovni1_bull3_done3
+scoreup3_bull3:
+   li $t2, -20
+   sw $t2, 36($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 88($sp) ;#pos bullet1
+
+ovni1_bull3_done3:
+
+;# 60,64, 72,76,  84,88, 96,100 bullets
+    ;#16,20 24,28, 36,40, 48,52 ovnis
+    ;#score 112
+
+bullet3_ovni4:
+   lw $t2, 48($sp) ;#posX ovni1
+   lw $t3, 52($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull3_y4
+   j ovni1_bull3_done4
+
+   check_bull3_y4:
+
+   beq $t1, $t3, scoreup4_bull3
+   beq $t1, $t4, scoreup4_bull3
+   beq $t1, $t5, scoreup4_bull3
+
+   j ovni1_bull3_done4
+scoreup4_bull3:
+   li $t2, -20
+   sw $t2, 48($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 88($sp) ;#pos bullet1
+
+ovni1_bull3_done4:
+
+ ;# 60,64, 72,76,  84,88, 96,100 bullets
+    ;#16,20 24,28, 36,40, 48,52 ovnis
+    ;#score 112
+
+    
+
+    
+   lw $t0, 96($sp) ;#posX bull 1
+   lw $t1, 100($sp)  ;#posY bull 1
+
+bullet4_ovni:
+   lw $t2, 16($sp) ;#posX ovni1
+   lw $t3, 20($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull4_y
+   j ovni1_bull4_done
+
+   check_bull4_y:
+
+   beq $t1, $t3, scoreup_bull4
+   beq $t1, $t4, scoreup_bull4
+   beq $t1, $t5, scoreup_bull4
+
+   j ovni1_bull4_done
+scoreup_bull4:
+   li $t2, -20
+   sw $t2, 16($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 100($sp) ;#pos bullet1
+
+ovni1_bull4_done:
+
+;# 60,64, 72,76,  84,88, 96,100 bullets
+    ;#16,20 24,28, 36,40, 48,52 ovnis
+    ;#score 112
+
+bullet4_ovni2:
+   lw $t2, 24($sp) ;#posX ovni1
+   lw $t3, 28($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull4_y2
+   j ovni1_bull4_done2
+
+   check_bull4_y2:
+
+   beq $t1, $t3, scoreup2_bull4
+   beq $t1, $t4, scoreup2_bull4
+   beq $t1, $t5, scoreup2_bull4
+
+   j ovni1_bull4_done2
+scoreup2_bull4:
+   li $t2, -20
+   sw $t2, 24($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 100($sp) ;#pos bullet1
+
+ovni1_bull4_done2:
+
+;# 60,64, 72,76,  84,88, 96,100 bullets
+    ;#16,20 24,28, 36,40, 48,52 ovnis
+    ;#score 112
+
+bullet4_ovni3:
+   lw $t2, 36($sp) ;#posX ovni1
+   lw $t3, 40($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull4_y3
+   j ovni1_bull4_done3
+
+   check_bull4_y3:
+
+   beq $t1, $t3, scoreup3_bull4
+   beq $t1, $t4, scoreup3_bull4
+   beq $t1, $t5, scoreup3_bull4
+
+   j ovni1_bull4_done3
+scoreup3_bull4:
+   li $t2, -20
+   sw $t2, 36($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 100($sp) ;#pos bullet1
+
+ovni1_bull4_done3:
+
+   ;# 60,64, 72,76,  84,88, 96,100 bullets
+    ;#16,20 24,28, 36,40, 48,52 ovnis
+    ;#score 112
+
+bullet4_ovni4:
+   lw $t2, 48($sp) ;#posX ovni1
+   lw $t3, 52($sp) ;#posy ovni1
+
+   li $t4, -1
+   add $t4, $t4, $t3
+   li $t5, 1
+   add $t5, $t5, $t3
+
+   beq $t0, $t2, check_bull4_y4
+   j ovni1_bull4_done4
+
+   check_bull4_y4:
+
+   beq $t1, $t3, scoreup4_bull4
+   beq $t1, $t4, scoreup4_bull4
+   beq $t1, $t5, scoreup4_bull4
+
+   j ovni1_bull4_done4
+scoreup4_bull4:
+   li $t2, -20
+   sw $t2, 48($sp) ;#posX ovni1
+   li $t2, -21
+   sw $t2, 100($sp) ;#pos bullet1
+
+ovni1_bull4_done4:
+
+
 
 j begin
 
